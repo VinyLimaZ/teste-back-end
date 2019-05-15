d9617d41bb393a7dcd1b8d3dd9414f2f4ec4ffdd
VinyLimaZ <vinylimaz@live.com>
2019-05-14 22:20:13 -0300

    Add AccessReport index
    - Add a serializer to properly return the json needed on the front
    - Add specs
    - Change some bussiness on User model

--- teste-back-end/config/routes.rb
+++ teste-back-end/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: :create
 
-  resources :accesses, only: %i[create index]
+  resources :accesses, only: :create
   get '/verify_uuid', to: 'accesses#verify_uuid'
+  get '/access_report', to: 'accesses#index'
 end
